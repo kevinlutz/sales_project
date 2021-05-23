c373ac7cae43440e75cb41651725f9c47aa333e7
kevinlutz <kevinlutz@gmail.com>
2021-05-23 13:36:45 -0400

    created sales project react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NavDrawer from './components/common/NavDrawer/NavDrawer'
+
+function App() {
+  return (
+    <div className="App">
+      <NavDrawer />
+    </div>
+  );
+}
+
+export default App;
